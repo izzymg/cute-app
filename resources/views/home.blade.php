3acc61f43ac402f2852c15430b924cf12f125676
izzymg <izzymhg@gmail.com>
2020-12-18 21:23:17 +1300

    Added pretty container to front page articles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,22 +16,30 @@
         </style>
     </head>
     <body class="">
+        @auth
         <div class="authed-banner">
-            @auth
-                <a href="{{ route('dash') }}" class="">Dashboard</a>
-            @endauth
+            <span class="logged-in-text">You're logged in. </span>
+            <a href="{{ route('dash') }}" class="">Dashboard</a>
         </div>
+        @endauth
         <main class="main">
-            <h1>Izzy MG</h1>
+            <h1 class="main-heading">Izzy MG</h1>
 
-            <h3>Posts</h3>
-            @foreach ($posts as $post)
-                <h4>
-                    <a href="{{ route('post', [$post->id]) }}">{{ $post->title }}</a>
-                </h4>
-                <span>{{ $post->pretty_date }}</span>
-                <p>{{ $post->text }}</p>
-            @endforeach
-            </main>
+            <h2 class="posts-heading">Posts</h2>
+            <div class="posts-grid">
+                <aside class="sidebar"/>
+                <div class="articles-container">
+                    @foreach ($posts as $post)
+                        <article class="post-wrap">
+                            <h3 class="post-date">Article {{ $post->pretty_date }}</h3>
+                            <h3 class="post-title">
+                                <a href="{{ route('post', [$post->id]) }}">{{ $post->title }}</a>
+                            </h3>
+                            <p class="post-desc">{{ $post->text }}</p>
+                        </article>
+                    @endforeach
+                </div>
+            </div>
+        </main>
     </body>
 </html>
