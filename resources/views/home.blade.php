c4f3e5354ade99adbf9cd696cdbabcdc3c337864
izzymg <izzymhg@gmail.com>
2020-12-19 11:23:25 +1300

    Move last css into files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,6 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/app.css">
-
-        <style>
-            body {
-                font-family: "Nunito";
-            }
-        </style>
     </head>
     <body class="">
         @auth
