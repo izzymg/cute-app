2de9a3e089698bf17efeeb3f7fc6dcd425adecee
izzymg <izzymhg@gmail.com>
2020-12-21 17:16:11 +1300

    Implement and test fetching all posts

--- app/Http/Repo/PostRepo.php
+++ app/Http/Repo/PostRepo.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Repo;
 
-use Exception;
+use Carbon\Carbon;
 
 use Illuminate\Support\Facades\DB;
 
 class PostRepo {
-    public $id = "";
-    public $title = "";
-    public $text = "";
+    public string $id;
+    public string $title;
+    public string $text;
+    public string $created_at;
 
-    public function __construct($id, $title, $text) {
+    public function __construct(string $id, string $title, string $text, string $created_at) {
         $this->id = $id;
         $this->title = $title;
         $this->text = $text;
+        $this->created_at = $created_at;
     }
 
     /**
@@ -28,5 +30,25 @@ class PostRepo {
             'created_at' => now(),
         ]);
         return $id;
+    }
+
+    /**
+    * Retrieves a post by its ID.
+    * @return PostRepo The post. 
+    */
+    public static function getById(string $id): PostRepo {
+        $post = DB::table('posts')->where('id', '=', $id)->first();
+        return new PostRepo($post->id, $post->title, $post->text, $post->created_at);
+    }
+
+    /**
+     * Retrieves all posts.
+     * @return array All posts.
+    */
+    public static function get(): array {
+        $posts = DB::table('posts')->get();
+        return $posts->collect()->transform(function($post) {
+            return new PostRepo($post->id, $post->title, $post->text, $post->created_at);
+        })->toArray();
     }
 }
--- tests/Feature/PostRepoTest.php
+++ tests/Feature/PostRepoTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use Carbon\Carbon;
+
 use App\Http\Repo\PostRepo;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -22,37 +24,78 @@ class PostRepoTest extends TestCase
         parent::tearDown();
     }
 
-    public function userProvider(): array {
+    public function postProvider(): array {
         return [
             [
-                '2',
-                'Jake',
-                'SomePasswordHash',
+                '1',
+                'xcvxcvvxc',
+                'ewtrtewtrewg',
+                '',
             ]
         ];
     }
 
     /**
      * Test creating a new post repo.
-     * @dataProvider userProvider
+     * @dataProvider postProvider
     */
-    public function testCreatePostRepo(string $id, string $title, string $text) {
-        $postRepo = new PostRepo($id, $title, $text);
+    public function testCreatePostRepo(string $id, string $title, string $text, string $created_at) {
+        $postRepo = new PostRepo($id, $title, $text, $created_at);
         $this->assertTrue($postRepo->id == $id);
         $this->assertTrue($postRepo->title == $title);
         $this->assertTrue($postRepo->text == $text);
+        $this->assertTrue($postRepo->created_at == $created_at);
     }
 
     /**
-     * Test saving a post or two.
+     * Test saving and retrieving post or two.
      * @depends testCreatePostRepo
-     * @dataProvider userProvider
+     * @dataProvider postProvider
     */
-    public function testSavePost(string $id, string $title, string $text) {
-        $postRepo = new PostRepo($id, $title, $text);
+    public function testSaveRetrievePost(string $id, string $title, string $text, string $created_at) {
+        $postRepo = new PostRepo($id, $title, $text, $created_at);
         $id = $postRepo->save();
         $this->assertTrue($id == '1');
+
+        $post = PostRepo::getById($id);
+        $this->assertTrue($post->id == $id);
+        $this->assertTrue($post->title == $title);
+        $this->assertTrue($post->text == $text);
+
         $id = $postRepo->save();
         $this->assertTrue($id == '2');
+
+        $post = PostRepo::getById($id);
+        $this->assertTrue($post->id == $id);
+        $this->assertTrue($post->title == $title);
+        $this->assertTrue($post->text == $text);
+    }
+
+    /**
+     * Test saving a few posts and getting them all back.
+     * @depends testCreatePostRepo
+    */
+    public function testSaveGetPosts() {
+        // DB auto increment doesn't get reset by RefreshDatabase, ugly hack
+        DB::table('posts')->truncate();
+
+        $posts = [
+            '1' => new PostRepo(1, 'ab', 'bd', ''),
+            '2' => new PostRepo(2, 'cd', 'fg', ''),
+            '3' => new PostRepo(3, 'bh', 'zg', ''),
+        ];
+
+        foreach ($posts as $id => $post) {
+            $post->save();
+        }
+
+        $fetchedPosts = PostRepo::get();
+
+        $this->assertTrue(count($posts) == count($fetchedPosts));
+
+        foreach ($fetchedPosts as $fetched) {
+            $this->assertTrue($posts[$fetched->id]->title == $fetched->title);
+            $this->assertTrue($posts[$fetched->id]->text == $fetched->text);
+        }
     }
 }
